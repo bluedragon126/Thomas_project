342ad42a927b036688e50819d318a7ebcdd6b604
Thomas Sandström <Thomas.Sandstrom@Borstjanaren.se>
2019-10-24 10:54:27 +0200

    AvaTrade

--- apps/frontend/modules/borst/templates/avaTradeSuccess.php
+++ apps/frontend/modules/borst/templates/avaTradeSuccess.php
@@ -22,9 +22,30 @@
         <div class="inner_page_divider">&nbsp;</div>
         <div class="float_l_width_100 mrg_left_70">
             <div class="inner_page_content_main">
+ <div class="whp_title" id="AVA"><?php echo __('AvaTrade') ?></div>
+ <div class="whp_preamble">AvaTrade är Börstjänarens sponsor och rekommenderade mäklare för CFD-handel, som vi samarbetat med kontinuerligt  alltsedan 2010. </div>
 
-
-                <?php echo include_partial('global/bottom_footer_whitepage'); ?>
+                <a class="main_link_color" href="https://www.avatrade.se/?fbg=2&tag=8947">AvaTrade</a> är en världsledande nätmäklare som erbjuder handel med mer än 200 Forex- och CFD-instrument, inklusive aktier, index, råvaror, obligationer &amp; ETF:er.<br />
+                    <br />
+                    <strong>Med över 200 000</strong> registrerade kunder från över 120 länder och reglering i sex jurisdiktioner, är AvaTrade en verkligt global mäklare.<br />
+                    <br />
+                    <strong>Dess framgång vilar</strong> på förmågan att leverera de produkter och tjänster som efterfrågas av kunniga handlare, såsom säkerheten i världsvid reglering, oöverträffad kapitalsäkerhet, utmärkt likviditet och en proaktiv kundsupport.<br />
+                    <br />
+                    <strong>AvaTrade fortsätter att </strong>sträva efter den högsta standarden – något som har uppmärksammats gång på gång inom industrin. Ava har vunnit flera utmärkelser, inklusive Bästa Affiliateprogram och Bästa Mäklare. </br></br>
+                <strong>Därför väljer seriösa handlare AvaTrade:</strong><br />
+                <ul>
+                    <li>Handla valutor, aktier, index, ETF:er med mer</li>
+                   <li>Handel på PC &amp; mobil med AvaTrader och MT4</li>
+                    <li>Utbud av automatiska handelsplattformar, signaler &amp;</li>
+                    <li>Expert Advisor-mjukvara</li>
+                    <li>Snabb, pålitligt exekvering, hög likviditet samt hävstång på upp till 400:1</li>
+                   <li>Fullständigt EU-reglerad och efterlever MiFID-direktiven</li>
+                   <li>Pålitlig proffsmäklare – global partner till Manchester City Football Club</li>
+              </ul>
+              
+ <strong>Öppna konto hos AvaTrade </strong> via Börstjönaren och få excellent, personlig kundsupport via vår specielle kundansvarige mäklare Mark Mates hos AvaTrade!               
+                    <p><a href="https://www.avatrade.se/?fbg=2&tag=8947"><img style="margin: 12px 0px 70px 0px;" src="../../../../../../images/new_home/open_AT.png" alt="http://www.pursegainer.com/images/new_home/Öppna konto hos Ava Trade här!.png" width="310;" /></a></p>
+              <img src="../../../../../images/new_home/PEP_728p925713648_cut.png" width="572" height="90" alt="Ava_MCI" longdesc="../../../../../images/new_home/PEP_728p925713648_cut.png" /> <?php echo include_partial('global/bottom_footer_whitepage'); ?>
             </div>
         </div>
         <div class="inner_page_divider_3">&nbsp;</div>
